380ba46bc33338826a7fdb225712eba0125ae5cc
Santhoshi-Assignments <160596233+Santhoshi-Assignments@users.noreply.github.com>
2024-03-11 12:20:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import CrudApp from './components/CrudApp';
-import jsonData from './components/Data.json'
-
-
 function App() {
   return (
     <div className="App">
